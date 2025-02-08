7b1ead5c0077b7302a304249fba2e4bbd5d0b0a6
eyal <eyalz2007@gmail.com>
2025-02-08 19:03:31 +0200

    adding / api

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //define imports
 const express = require("express");
+const mongoose = require("mongoose");
 app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-    console.log("My App is listening on port 3000");
+
+app.get('/', (request, response) => {
+    console.log("GET /");
+    console.log("its working!")
+    response.send(request.body);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
 })
